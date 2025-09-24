da320be79133ba839e6b84dea661cb45654ec18b
Felix Vossel <fvossel@uos.de>
2025-09-24 20:51:24 +0200

    fixed typo

--- train.py
+++ train.py
@@ -9,14 +9,14 @@ from utils.training import train
 
 
 def main(args):
-    model, tokenizer = initialize_model_and_tokenizer_for_training(args.model_name, ft_setting=args.ft_settings)
-    train_dataset, validation_dataset = load_datasets_for_training(args.model_name, args.ft_settings, tokenizer)
-    train(model, args.model_name, args.ft_settings, train_dataset, validation_dataset)
+    model, tokenizer = initialize_model_and_tokenizer_for_training(args.model_name, ft_setting=args.ft_setting)
+    train_dataset, validation_dataset = load_datasets_for_training(args.model_name, args.ft_setting, tokenizer)
+    train(model, args.model_name, args.ft_setting, train_dataset, validation_dataset)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Training Script for NL2FOL")
     parser.add_argument('--model_name', type=str, required=True, help='Name of the pre-trained model')
-    parser.add_argument('--ft_settings', type=str, required=True, help='Fine-tuning settings')
+    parser.add_argument('--ft_setting', type=str, required=True, help='Fine-tuning setting')
     args = parser.parse_args()
     main(args)
